f26ad2fa2dd3a7b01f944852c3ef4674e2044f7d
Andrei Ignat <andrei@ignat.se>
2022-02-26 20:05:22 +0100

    First

---
+++ extras/tool.py
@@ -0,0 +1,21 @@
+# Tool support
+#
+# Copyright (C) 2022  Andrei Ignat <andrei@ignat.se>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+import logging
+#import pins
+#from . import probe
+#from . import manual_probe
+
+#HINT_TIMEOUT = """
+#If the probe did not move far enough to trigger, then
+#consider reducing the Z axis minimum position so the probe
+#can travel further (the Z minimum position can be negative).
+#"""
+
+class Tool:
+    def __init__(self, printer, mcu):
+
+class ToolLock:
+    def __init__(self, printer, mcu):
