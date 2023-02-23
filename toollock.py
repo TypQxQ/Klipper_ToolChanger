d2213003d14334c5cd25881b474d3f2dd2d59367
Andrei Ignat <andrei@ignat.se>
2023-02-23 07:57:09 +0100

    Update toollock.py

--- toollock.py
+++ toollock.py
@@ -1,12 +1,18 @@
 # KTCC - Klipper Tool Changer Code
-# Log and statistics module
+# Main module
 #
 # Copyright (C) 2023  Andrei Ignat <andrei@ignat.se>
 #
-# Based on and inspired by ERCF-Software-V3      Copyright (C) 2021  moggieuk#6538 (discord)
-#
 # This file may be distributed under the terms of the GNU GPLv3 license.
 #
+
+# To try to keep terms apart:
+# Mount: Tool is selected and loaded for use, be it a physical or a virtual on physical.
+# Unmopunt: Tool is unselected and unloaded, be it a physical or a virtual on physical.
+# Pickup: Tool is physically picked up and attached to the toolchanger head.
+# Droppoff: Tool is physically parked and dropped of the toolchanger head.
+# ToolLock: Toollock is engaged.
+# ToolUnLock: Toollock is disengaged.
 
 class ToolLock:
     TOOL_UNKNOWN = -2
